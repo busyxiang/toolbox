6bce17295cc1f0d8dd3cc27199ed05831175620a
Ray Foo <ray@6biz.ai>
2022-11-04 12:34:17 +0800

    Add atrribute property to useInjectScript hook

--- packages/react/src/useInjectScript.ts
+++ packages/react/src/useInjectScript.ts
@@ -1,20 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 type UseInjectScriptProps = {
   url: string;
+  attributes?: Record<string, string>;
   onLoad?: () => void;
 };
 
 export const useInjectScript = (props: UseInjectScriptProps) => {
-  const { url, onLoad } = props;
+  const { url, attributes, onLoad } = props;
 
   const [loaded, setLoaded] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
-    const script = document.createElement("script");
+    const script = document.createElement('script');
     script.src = url;
     script.async = true;
+
+    if (attributes && Object.keys(attributes).length > 0) {
+      Object.entries(attributes).forEach(([key, value]) => {
+        script.setAttribute(key, value);
+      });
+    }
 
     script.onload = () => {
       onLoad?.();
